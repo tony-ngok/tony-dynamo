19160685a4da8bfb6cb3b78189be977f22c82cd3
tony-ngok <tonyn1999@outlook.com>
2025-06-25 16:14:51 -0400

    start on dynamodb

---
+++ app/models/_dynamooseConfig.js
@@ -0,0 +1,17 @@
+import dynamoose from 'dynamoose'
+
+const ddb = new dynamoose.aws.ddb.DynamoDB({
+  credentials: {
+    accessKeyId: process.env.PRIVATE_AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.PRIVATE_AWS_SECRET_ACCESS_KEY
+  },
+  region: PRIVATE_AWS_REGION
+})
+
+dynamoose.aws.ddb.set(ddb)
+dynamoose.Table.defaults.set({
+  create: false,
+  waitForActive: { enabled: false }
+})
+
+export const TableName = 'TonyTable'
