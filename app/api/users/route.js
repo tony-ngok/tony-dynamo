a70327302d8fe06964335acaa9281f1740e29106
tony-ngok <tonyn1999@outlook.com>
2025-06-27 15:03:39 -0400

    sort asc && desc

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -1,9 +1,12 @@
 import UserModel from "@/app/models/UserModel"
 
 export async function GET(request) {
+  const url = new URL(request.url)
+  const sort = url.searchParams.get('sort') || 'ascending'
+
   try {
-    let res = await UserModel.query().where('GSI1PK').eq('USER').using('nameIndex').sort('ascending').exec()
-    console.log(res)
+    let res = await UserModel.query().where('GSI1PK').eq('USER').using('nameIndex').sort(sort).exec()
+    // console.log(res)
     return Response.json({ data: res }, { status: 200 })
   } catch (err) {
     console.log(err)
--- app/page.js
+++ app/page.js
@@ -5,10 +5,13 @@ import { useEffect, useState } from "react"
 export default function App() {
   const [users, setUsers] = useState(undefined)
   const [hasError, setHasError] = useState(false)
+  const [isDesc, setIsDesc] = useState(false)
 
   useEffect(() => {
+    const sort = isDesc ? 'descending' : 'ascending'
+
     async function getUsers() {
-      const res = await fetch("/api/users")
+      const res = await fetch(`/api/users?sort=${sort}`)
       if (res.ok) {
         setUsers((await res.json()).data)
       } else {
@@ -16,12 +19,19 @@ export default function App() {
       }
     }
     getUsers()
-  }, [])
+  }, [isDesc])
 
   if (hasError) return <div>出错了，刷新一下吧</div>
 
   return (
     <>
+      <h1>全部用户</h1>
+
+      <label>
+        <input type="checkbox" checked={isDesc} onChange={() => setIsDesc(!isDesc)} disabled={users === undefined} />
+        降序排列
+      </label>
+
       {users && users.length && users.map((user) =>
         <div key={user.pk}>{user.name} ({user.email})</div>
       )}
