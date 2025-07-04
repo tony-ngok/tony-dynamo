ca9f497bd70b8d11cdf15ba9207f5ba1f4c8e0a2
tony-ngok <tonyn1999@outlook.com>
2025-07-04 11:52:46 -0400

    加上一个模型

---
+++ app/models/DirModel.js
@@ -0,0 +1,27 @@
+import dynamoose from 'dynamoose'
+import { TableName } from './_dynamooseConfig'
+
+const DirSchema = new dynamoose.Schema({
+  pk: {
+    type: String,
+    hashKey: true
+  },
+  sk: {
+    type: String,
+    rangeKey: true
+  },
+  GSI1PK: {
+    type: String,
+    index: {
+      name: 'nameIndex',
+      type: 'global',
+      rangeKey: 'GS1SK'
+    }
+  },
+  GSI1SK: String,
+  dirName: String
+})
+
+const DirModel = dynamoose.model("Dir", DirSchema)
+const _ = new dynamoose.Table(TableName, [DirModel])
+export default DirModel
